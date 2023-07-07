1bf9d3f80519789d01220fed1a92c7f91c769621
devairjr <devair@ymail.com>
2023-07-07 18:17:16 -0300

    Exemplo de reflexão, parei na primeira refatoração

---
+++ reflection/reflection.go
@@ -0,0 +1,12 @@
+package reflection
+
+import "reflect"
+
+func percorre(x interface{}, fn func(entrada string)) {
+	valor := reflect.ValueOf(x)
+
+	for i := 0; i < valor.NumField(); i++ {
+		campo := valor.Field(i)
+		fn(campo.String())
+	}
+}
---
+++ reflection/reflection_test.go
@@ -0,0 +1,47 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPercorre(t *testing.T) {
+
+	casos := []struct {
+		Nome              string
+		Entrada           interface{}
+		ChamadasEsperadas []string
+	}{
+		{
+			"Struct com um campo string",
+			struct {
+				Nome string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct com dois campo string",
+			struct {
+				Nome   string
+				Cidade string
+			}{"Chris", "Londres"},
+			[]string{"Chris", "Londres"},
+		},
+	}
+
+	for _, teste := range casos {
+		t.Run(teste.Nome, func(t *testing.T) {
+			var resultado []string
+			percorre(teste.Entrada, func(entrada string) {
+				resultado = append(resultado, entrada)
+			})
+
+			if !reflect.DeepEqual(resultado, teste.ChamadasEsperadas) {
+				t.Errorf("resultado %v, esperado %v", resultado, teste.ChamadasEsperadas)
+			}
+		})
+	}
+}
+
+// parei aqui (07/07/2023)
+// https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/reflection#refatoracao-1
