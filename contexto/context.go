8969989ed95ad733b0a8e514409666add77735bb
devairjr <devair@ymail.com>
2023-07-18 11:39:08 -0300

    Continuação exemplo contexto

--- contexto/context.go
+++ contexto/context.go
@@ -1,30 +1,23 @@
 package contexto
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		data, _ := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
+		// if err != nil {
+		// 	return // todo: registre o erro como vc quiser
+		// }
 
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
-		}
+		fmt.Fprint(w, data)
 	}
 }
--- contexto/context_test.go
+++ contexto/context_test.go
@@ -1,11 +1,9 @@
 package contexto
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 )
 
 func TestHandler(t *testing.T) {
@@ -23,26 +21,22 @@ func TestHandler(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf("resultado '%s', esperado '%s'", response.Body.String(), data)
 		}
-
-		store.assertWasNotCancelled()
 	})
 
-	t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada", func(t *testing.T) {
-		store := &SpyStore{response: data, t: t}
-		svr := Server(store)
+	// t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada", func(t *testing.T) {
+	// 	store := &SpyStore{response: data, t: t}
+	// 	svr := Server(store)
 
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
+	// 	request := httptest.NewRequest(http.MethodGet, "/", nil)
 
-		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5*time.Millisecond, cancel)
-		request = request.WithContext(cancellingCtx)
+	// 	cancellingCtx, cancel := context.WithCancel(request.Context())
+	// 	time.AfterFunc(5*time.Millisecond, cancel)
+	// 	request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+	// 	response := httptest.NewRecorder()
 
-		svr.ServeHTTP(response, request)
+	// 	svr.ServeHTTP(response, request)
 
-		store.assertWasCancelled()
-	})
+	// 	store.assertWasCancelled()
+	// })
 }
-
-//https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/contexto#escreva-o-teste-primeiro-1
